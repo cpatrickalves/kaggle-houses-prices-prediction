4bf0cbd22861040b8f6c75b8c69da8e9b320b9d5
Patrick Alves <cpatrickalves@gmail.com>
2020-06-06 14:03:29 -0300

    Refactoring: - move load_model function to the HousePriceModel class - Change the parameters of the model - Saves de model inside a folder in models/ - create methods to get examples and input features of the model - Add some prints to show the progress - Update the export_model function to save in de model folder

--- src/models/predict.py
+++ src/models/predict.py
@@ -1,32 +1,70 @@
 import pickle
 import os
 import pandas as pd
-from train_model import load_data
 
 class HousePriceModel():
-    def __init__(self, model):
-        self.model = model
+
+    def __init__(self, model_dir, models_path='../../models'):
+        """Create the HousePriceModel object
+
+        Args:
+            model_dir (str): directory where the model is stored
+            models_path (str): path to models directory
+        """
+        self.path = os.path.join(models_path, model_dir)
+        self.model = self.load_model()
         self.preds = None
+
+    def load_model(self):
+        pkl_filename = os.path.join(self.path, 'model.pkl')
+
+        try:
+            with open(pkl_filename, 'rb') as file:
+                pickle_model = pickle.load(file)
+        except:
+            print(f'Error loading the model at {pkl_filename}')
+            return None
+
+        return pickle_model
 
     def predict(self, data):
         dataDF = pd.DataFrame.from_dict(data)
         self.preds = self.model.predict(dataDF)
         return self.preds
 
-    
-def load_model(path, name):
-    pkl_filename = os.path.join(path, name)
+    def get_input_example(self):
+        """Get an input example for the model
 
-    with open(pkl_filename, 'rb') as file:
-        pickle_model = pickle.load(file)
+        Returns:
+            Dataframe: a dataframe with a set of examples
+        """
+        input_template = os.path.join(self.path, "test_input.csv")
+        input_example = pd.read_csv(input_template)
 
-    return pickle_model
+        return input_example
+
+    def get_input_features(self):
+        """Get the input features for the model
+
+        Returns:
+            list: a list with all features expected by the model
+        """
+        input_features = self.get_input_example().columns
+
+        return list(input_features)
+
 
 def main():
-    model_pkl = load_model('../../models/', 'tree_model.pkl')
-    model = HousePriceModel(model_pkl)
-    test = load_data('../../models/test_input.csv')
-    print(model.predict(test.to_dict()))
+
+    model_dir = 'tree_model'
+
+    print(f"Loading model: {model_dir}")
+    model = HousePriceModel(model_dir)
+    test = model.get_input_example()
+
+    input_features = model.get_input_features()
+    print(f"Input features: {input_features}")
+    print(f"Predictions: {model.predict(test.to_dict())}")
 
 if __name__ == '__main__':
     main()
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -5,83 +5,109 @@ from sklearn.metrics import mean_squared_error
 from sklearn.tree import DecisionTreeRegressor
 import os
 import pickle
-
 import sys
 sys.path.append('../')
-
 from data import pre_processing
 
+
 def load_data(dataset_path):
+    print(f"Loading the data at {dataset_path}")
     data = pd.read_csv(dataset_path)
     return data
 
+
 def remove_columns_small_correlation(train_data):
     corr_matrix = train_data.corr()
-
     correlations = corr_matrix["SalePrice"]
-
     uncorrelated_columns = correlations[correlations < 0.3]
-
     train_data = train_data.drop(columns=uncorrelated_columns.index)
 
     return train_data
 
+
 def encode_categorical_columns(train_data):
     categorical_mask = train_data.dtypes==object
-
     categorical = train_data.columns[categorical_mask].tolist()
-
     le = LabelEncoder()
-
     train_data[categorical] = train_data[categorical].apply(lambda col: le.fit_transform(col))
 
     return train_data
 
+
 def prepare_data(train_data):
+    print(f"Performing data cleaning ...")
     train_data = pre_processing.clean_data(train_data)
+    print(f"Performing feature selection ...")
     train_data = remove_columns_small_correlation(train_data)
+    print(f"Performing data pre-processing ...")
     train_data = encode_categorical_columns(train_data)
 
     return train_data
 
-def save_data(dataframe, path, name):
-    dataframe.to_csv(os.path.join(path, name), index=False)
 
 def train_model(x_train, y_train):
+    print("Training the model ...")
     tree_reg = DecisionTreeRegressor()
     tree_reg.fit(x_train, y_train)
-    
+
     return tree_reg
 
+
 def accuracy(model, x_test, y_test):
+    print("Testing the model ...")
     predictions = model.predict(x_test)
     tree_mse = mean_squared_error(y_test, predictions)
     tree_rmse = np.sqrt(tree_mse)
     return tree_rmse
 
-def export_model(model, path, name):
-    pkl_filename = os.path.join(path, name)
 
-    with open(pkl_filename, 'wb') as file:
+def export_model(model, inputs_df, output_dir='last_model', path='../../models/'):
+    """Saves model as PKL file and an example of the input data as a CSV file
+
+    Args:
+        model (object): trained model
+        inputs_df (DataFrame): a dataframe with a set of examples
+        output_dir (str, optional): The directory where the files should be saved. Defaults to 'last_model'.
+        path (str, optional): path where the output_dir should be created. Defaults to '../../models/'.
+    """
+
+    # Create directory if not exist
+    save_dir = os.path.join(path, output_dir)
+    if not (os.path.exists(save_dir)):
+        os.mkdir(save_dir)
+
+    # Save the model
+    pkl_path = os.path.join(save_dir, 'model.pkl')
+    with open(pkl_path, 'wb') as file:
         pickle.dump(model, file)
+        print(f"Model saved at {pkl_path}")
+
+    # Save a set of examples of train data
+    examples_path = os.path.join(save_dir, 'test_input.csv')
+    inputs_df.to_csv(examples_path, index=False)
+    print(f"Input examples saved at {examples_path}")
+
 
 def main():
+    print("-"*20, "Starting", "-"*20)
+    # Load data
     train = load_data('../../data/raw/train.csv')
-
     train = prepare_data(train)
 
+    # Split train test
     x_train = train.drop(columns=['SalePrice'])
     y_train = train['SalePrice']
 
+    # Train and Test
     model = train_model(x_train, y_train)
-
     rmse = accuracy(model, x_train, y_train)
 
-    print(rmse)
+    print(f"RMSE: {rmse}")
 
-    export_model(model, '../../models/', 'tree_model.pkl')
-    
-    save_data(x_train.head(5), '../../models/', 'test_input.csv')
+    # Save the model and input data
+    export_model(model, x_train.head(5), output_dir='tree_model')
+
+    print("-"*20, "Done", "-"*20)
 
 if __name__ == '__main__':
     main()
