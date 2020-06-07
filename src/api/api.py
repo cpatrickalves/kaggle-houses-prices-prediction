ad67f79c743f13198fb4e51d80f0ffae77ce5bdb
Patrick Alves <cpatrickalves@gmail.com>
2020-06-07 16:07:37 -0300

    Create predict endpoint

--- src/api/api.py
+++ src/api/api.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 import pandas as pd
 from logzero import logger
+from pydantic import BaseModel
+from typing import Dict
+import json
+import sys
+sys.path.append('../')
+from models import predict
+
 
 # ------------------------ HOW TO USE ------------------------ #
 # To see API documentation, start the API and access:
@@ -12,10 +19,44 @@ from logzero import logger
 
 app = FastAPI()
 
+logger.info("API started")
+
+#TODO: Use BaseModel for automatic validation
+#class JsonBody(BaseModel):
+#    name: str
+
+# Create a Basemodel from dict?
+#class ModelInputs(BaseModel):
+#    SaleType : str = None
+#    OpenPorchSF : str = None
+#    WoodDeckSF : str = None
+#    SaleCondition : str = None
+
 @app.get("/")
-def api_status():
+def root():
     """
     Root view returns the system status
     """
     return {"status": "online"}
 
+
+@app.post("/predict")
+def predict(inputs: dict):
+
+    #logger.info(f"API - POST /predict: \n {json.dumps(inputs, indent=4)}")
+
+    # set the model saved at models/
+    model_dir = 'tree_model'
+
+    #print(f"Loading model: {model_dir}")
+    #model = HousePriceModel(model_dir)
+
+    # Get one input example as dict
+    #test_df = model.get_input_example()
+    #sample_input = test_df.to_dict(orient='records')[0]
+
+    #input_features = model.get_input_features()
+    #response = f"Prediction: {model.predict(sample_input)}")
+
+    #return response
+    return inputs
--- src/api/test_api.py
+++ src/api/test_api.py
@@ -1,13 +1,47 @@
 from starlette.testclient import TestClient
 from api import app
+import requests
 
 client = TestClient(app)
 
-def test_status_code():
+def test_root():
     response = client.get("/")
     assert response.status_code == 200
-
-def test_status_response():
-    response = client.get("/")
     assert response.json() == {"status": "online"}
 
+def test_predict():
+    response = client.post(
+        "/predict",
+        json={"SaleType": "foobar",
+              "OpenPorchSF": "Foo Bar",
+              "SaleCondition": "The Foo Bar"},
+    )
+    assert response.status_code == 200
+    assert response.json() == {"SalesPrice": 42}
+
+
+def run_prediction_from_sample(model_dir):
+    """Send a request to the API using a sample from the model
+
+    Args:
+        model_dir (str): model folder
+    """
+
+    # set the model saved at models
+    #logger.info(f"Loading model: {model_dir}")
+    #model = HousePriceModel(model_dir)
+
+    # Get one input example as dict
+    #test_df = model.get_input_example()
+    #sample_input = test_df.to_dict(orient='records')[0]
+
+    # Run send a predict request with the sample
+    url="http://127.0.0.1:8000/predict"
+    headers = {"Content-Type": "application/json", "Accept":"text/plain"}
+    sample_input = {"SaleType": "Somevalue01", "WoodDeckSF": "42", "SaleCondition":22}
+    response = requests.post(url, headers=headers, json=sample_input)
+    print(response.text)
+
+
+if __name__ == "__main__":
+    run_prediction_from_sample('tree_model')
