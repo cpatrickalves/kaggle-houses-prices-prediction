61e5928018a120e364dace2b7b53bff9fa0b2f81
Igor Couto <igorcouto@gmail.com>
2020-06-10 08:26:34 -0300

    change dict format

--- src/app.py
+++ src/app.py
@@ -4,16 +4,17 @@ import streamlit as st
 # Project
 from models import predict
 
-model = predict.HousePriceModel(r'D:\Dropbox\ds_pratica\kaggle-houses-prices-prediction\models\tree_model')
+model = predict.HousePriceModel(r'C:\Users\84554037\Dropbox\ds_pratica\kaggle-houses-prices-prediction\models\tree_model')
 
 
 # Sidebar
 st.sidebar.title('Input')
 input_examples = model.get_input_example()
-input_example = input_examples.sample().to_dict('list')
+input_example = input_examples.sample().to_dict('r')
 
-for k, v in input_example.items():
-    st.sidebar.number_input(k, value=v[0])
+for i in input_example:
+    for k, v in i.items():
+        st.sidebar.number_input(k, value=v)
 
 # Main panel
 st.title('House Prices Preditor')
